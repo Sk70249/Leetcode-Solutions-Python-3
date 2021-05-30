6ad0c4bcea301ac3ba76a59126301f6806834385
Samyak <sam2452344@gmail.com>
2021-05-30 19:28:57 +0530

    Create ValidParantheses.py

---
+++ Easy/ValidParantheses.py
@@ -0,0 +1,29 @@
+# @Sk70249 Reverse Integer solution in Python
+'''
+Example 1:
+Input: s = "()[]{}"
+Output: true
+
+Example 2:
+Input: s = "(]"
+Output: false
+
+Example 3:
+Input: s = "([)]"
+Output: false
+'''
+class Solution:
+    def isValid(self, s: str) -> bool:
+        l = []
+        bracket = {")": "(",
+                 "}": "{",
+                 "]": "["}
+
+        for i in s:
+            if(i in ["(", "{", "["]):
+                l.append(i)
+            elif(len(l) == 0 or bracket[i] != l.pop()):
+                return 0
+
+        return l == []
+
